0857169a85d0395b401fd569b44008e0a70c6b2b
GavinAtCWU <Gavin.Spomer@cwu.edu>
2025-11-19 15:39:05 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+<title>Textbooks In Primo (TIP)</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+<h1 class="page-title"><b>T</b>extbooks <b>I</b>n <b>P</b>rimo</h1>
+
+</body>
+
+</html>
