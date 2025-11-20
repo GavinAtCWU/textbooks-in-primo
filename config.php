c8320286908f56cd96d3b924220d4f6352273119
GavinAtCWU <Gavin.Spomer@cwu.edu>
2025-11-20 10:32:00 -0800

    More progress, new files.

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
 
+// MySQL setup, change as necessary
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'mysql-user-with-privileges-for-database');
+define('MYSQL_PASS', 'mysql-user-password');
+define('MYSQL_DB', 'mysql-database-name');
+
+// CSV column mappings
 
 
 ?>
---
+++ db_connect.php
@@ -0,0 +1,7 @@
+<?php
+
+include('config.php');
+
+$mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+
+?>
